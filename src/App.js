0a27d92e3c5d86ac119acbe8a60763a214847967
Emil <emil.varn@gmail.com>
2021-05-03 19:39:00 +0300

    Style App with Material Design

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import {Provider} from 'react-redux';
+import {createMuiTheme} from '@material-ui/core/styles';
+import green from '@material-ui/core/colors/green';
+import {ThemeProvider} from '@material-ui/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 import store from './store';
 import {RestaurantScreen} from './components/RestaurantScreen';
 
-function App() {
-  return (
-    <Provider store={store}>
-      <RestaurantScreen />
-    </Provider>
-  );
-}
+const theme = createMuiTheme({
+  palette: {
+    primary: green,
+  },
+});
+
+const App = () => (
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">Opinion Ate</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container>
+        <RestaurantScreen />
+      </Container>
+    </ThemeProvider>
+  </Provider>
+);
 
 export default App;
--- src/components/RestaurantScreen.js
+++ src/components/RestaurantScreen.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 import RestaurantList from './RestaurantList';
 
-export const RestaurantScreen = () => {
-  return (
-    <div>
-      <h1>Restaurants</h1>
+export const RestaurantScreen = () => (
+  <Card>
+    <CardContent>
+      <Typography variant="h5">Restaurants</Typography>
       <RestaurantList />
-    </div>
-  );
-};
+    </CardContent>
+  </Card>
+);
