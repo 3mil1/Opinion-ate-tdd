a4fd6cca1526cdfdb00d4fec2dc853f71b2a962b
Emil <emil.varn@gmail.com>
2021-05-04 09:17:12 +0300

    Error popup

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -4,25 +4,40 @@ import {loadRestaurants} from '../store/restaurants/actions';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Alert from '@material-ui/lab/Alert';
 
-export const RestaurantList = ({loadRestaurants, restaurants}) => {
+export const RestaurantList = ({
+  loadRestaurants,
+  restaurants,
+  loading,
+  loadError,
+}) => {
   useEffect(() => {
     loadRestaurants();
   }, [loadRestaurants]);
 
   return (
-    <List>
-      {restaurants.map(restaurant => (
-        <ListItem key={restaurant.id}>
-          <ListItemText>{restaurant.name}</ListItemText>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      {loading && <CircularProgress data-testid="loading-indicator" />}
+      {loadError && (
+        <Alert severity="error">Restaurants could not be loaded.</Alert>
+      )}
+      <List>
+        {restaurants.map(restaurant => (
+          <ListItem key={restaurant.id}>
+            <ListItemText>{restaurant.name}</ListItemText>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
 const mapStateToProps = state => ({
   restaurants: state.restaurants.records,
+  loading: state.restaurants.loading,
+  loadError: state.restaurants.loadError,
 });
 
 const mapDispatchToProps = {loadRestaurants};
